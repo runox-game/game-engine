bf12473d642accc09de675336c822052846e13b4
AlexScigalszky <alexpablo.90@gmail.com>
2020-05-28 16:58:37 -0300

    add new levels

--- src/log/log-levels.enum.ts
+++ src/log/log-levels.enum.ts
@@ -5,6 +5,8 @@ export enum LogLevel {
   DEFAULT = 'default',
   ALL = 'all',
   USER = 'user',
-  BEFORE_COMMAND = 'before',
-  AFTER_COMMAND = 'after',
+  BEFORE_COMMAND = 'before-command-execute',
+  AFTER_COMMAND = 'after-command-execute',
+  BEFORE_VALIDATION = 'before-command-validation',
+  AFTER_VALIDATION = 'after-command-validation',
 }
